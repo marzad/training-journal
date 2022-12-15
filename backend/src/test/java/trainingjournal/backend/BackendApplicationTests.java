001a7ed484d3ecc5c0f524a139d7586c0273f15b
marzad <maschatz@gmx.net>
2022-12-15 15:52:15 +0100

    Change BackendApplicationTests

--- backend/src/test/java/trainingjournal/backend/BackendApplicationTests.java
+++ backend/src/test/java/trainingjournal/backend/BackendApplicationTests.java
@@ -3,11 +3,14 @@ package trainingjournal.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
