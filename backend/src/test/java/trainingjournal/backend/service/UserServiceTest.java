3550d1334367b0e3032451541f8bb86e32645ce8
marzad <maschatz@gmx.net>
2023-01-05 17:37:47 +0100

    Rename method, add new test & remove useless import

--- backend/src/test/java/trainingjournal/backend/service/UserServiceTest.java
+++ backend/src/test/java/trainingjournal/backend/service/UserServiceTest.java
@@ -2,7 +2,6 @@ package trainingjournal.backend.service;
 
 import org.junit.jupiter.api.Test;
 import trainingjournal.backend.model.*;
-import trainingjournal.backend.repository.ExerciseRepository;
 import trainingjournal.backend.repository.UserRepository;
 
 import java.util.*;
@@ -14,25 +13,7 @@ import static org.mockito.Mockito.when;
 class UserServiceTest {
 
     UserRepository userRepository = mock(UserRepository.class);
-    ExerciseRepository exerciseRepository = mock(ExerciseRepository.class);
-    UserService userService = new UserService(userRepository, exerciseRepository);
-
-
-
-    //JimUser(id,
-    //                newUser.username(),
-    //                gender,
-    //                birthday,
-    //                weight,
-    //                new HashSet<Week>(Set.of()),
-    //                LocalDate.now(),
-    //                password);
-    // addUser(JimUserDTO newUser, Gender gender, LocalDate birthday, float weight)
-
-    @Test
-    void test_something(){
-        assertTrue(true);
-    }
+    UserService userService = new UserService(userRepository);
 
     @Test
     void test_addUserExercisesList(){
@@ -41,13 +22,13 @@ class UserServiceTest {
         user.setExercises(new HashSet<>());
 
         Exercise newExercise = new Exercise("1", "newExercise", 0,0,0);
-        List<Exercise> newList = new ArrayList<>();
-        newList.add(newExercise);
-        Set<Exercise> newSet = new HashSet<>(newList);
+        List<Exercise> newExercisesList = new ArrayList<>();
+        newExercisesList.add(newExercise);
+        Set<Exercise> newSet = new HashSet<>(newExercisesList);
 
         when(userRepository.findByUsername("username")).thenReturn(user);
 
-        Set<Exercise> result = userService.addUserExercisesList("username", newList);
+        Set<Exercise> result = userService.addUserExercisesList("username", newExercisesList);
 
         assertEquals(newSet, result);
 
