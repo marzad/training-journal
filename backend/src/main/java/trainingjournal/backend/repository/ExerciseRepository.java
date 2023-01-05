cc2ef1adac0c11e64e9a264974047671efc18afd
marzad <maschatz@gmx.net>
2023-01-05 17:48:20 +0100

    Change details

--- backend/src/main/java/trainingjournal/backend/repository/ExerciseRepository.java
+++ backend/src/main/java/trainingjournal/backend/repository/ExerciseRepository.java
@@ -1,14 +1,11 @@
 package trainingjournal.backend.repository;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import trainingjournal.backend.model.Exercise;
+import trainingjournal.backend.model.ExerciseDTO;
 
 import java.util.Optional;
-
-@Document("Exercises")
 @Repository
-public interface ExerciseRepository extends MongoRepository<Exercise, String> {
-    Optional<Exercise> findByDescription(String exerciseName);
+public interface ExerciseRepository extends MongoRepository<ExerciseDTO, String> {
+    Optional<ExerciseDTO> findByDescription(String exerciseName);
 }
