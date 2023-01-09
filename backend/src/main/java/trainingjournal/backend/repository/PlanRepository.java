6ae641b9902d97b0a449202c242eeca92aa6d518
marzad <maschatz@gmx.net>
2023-01-09 14:59:40 +0100

    Update files

--- backend/src/main/java/trainingjournal/backend/repository/PlanRepository.java
+++ backend/src/main/java/trainingjournal/backend/repository/PlanRepository.java
@@ -1,10 +1,10 @@
 package trainingjournal.backend.repository;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import trainingjournal.backend.model.Week;
-@Document("Plans")
+
+
 @Repository
 public interface PlanRepository extends MongoRepository<Week, String> {
 }
