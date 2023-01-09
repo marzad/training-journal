c9885087b1688e0fd77dbd793f6730167551bea6
marzad <maschatz@gmx.net>
2023-01-09 15:01:53 +0100

    Add autoComplete to input field "password" parameters

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,37 +1,42 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import "../css/LoginPage.css"
 
 type LoginPageProps = {
     login: (username: string, password: string) => Promise<string>
 }
 
 
-export default function LoginPage(props: LoginPageProps){
+export default function LoginPage(props: LoginPageProps) {
 
     const [userName, setUserName] = useState<string>("anton")
     const [password, setPassword] = useState<string>("123")
 
     const navigate = useNavigate()
 
-    function handleUsernameOnChange(event: ChangeEvent<HTMLInputElement>){
+    function handleUsernameOnChange(event: ChangeEvent<HTMLInputElement>) {
         setUserName(event.target.value)
     }
 
-    function handlePasswordOnChange(event: ChangeEvent<HTMLInputElement>){
+    function handlePasswordOnChange(event: ChangeEvent<HTMLInputElement>) {
         setPassword(event.target.value)
     }
 
-    function onSubmit(event: FormEvent<HTMLFormElement>){
+    function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
         props.login(userName, password)
-            .then(() => navigate("menu"))
+            .then(() => navigate("/menu"))
     }
 
-    return(
+    return (
         <>
             <form onSubmit={onSubmit}>
                 <input type={"text"} value={userName} name={"userName"} onChange={handleUsernameOnChange}/><br/>
-                <input type={"password"} value={password} name={"password"} onChange={handlePasswordOnChange}/><br/>
+                <input type={"password"}
+                       value={password}
+                       name={"password"}
+                       onChange={handlePasswordOnChange}
+                       autoComplete={"123"}/><br/>
                 <button type={"submit"}>Login</button>
             </form>
         </>
