cc050338247405831da0527c20af31dac56038e1
marzad <maschatz@gmx.net>
2023-01-09 04:08:12 +0100

    Add bodysize to GymUser and update relevant methods

--- backend/src/test/java/trainingjournal/backend/service/UserServiceTest.java
+++ backend/src/test/java/trainingjournal/backend/service/UserServiceTest.java
@@ -22,10 +22,13 @@ class UserServiceTest {
         user.setUsername("username");
         user.setExercises(new HashSet<>());
 
-        Exercise newExercise = new Exercise("1", "newExercise", 0,0,0);
-        List<Exercise> newExercisesList = new ArrayList<>();
-        newExercisesList.add(newExercise);
-        Set<Exercise> newSet = new HashSet<>(newExercisesList);
+        ExerciseDTO newExerciseDTO = new ExerciseDTO("1", "newExercise");
+        List<ExerciseDTO> newExercisesList = new ArrayList<>();
+        newExercisesList.add(newExerciseDTO);
+
+        Exercise newExercise = new Exercise("1", "newExercise", 0,0,0.0);
+        Set<Exercise> newSet = new HashSet<>();
+        newSet.add(newExercise);
 
         when(userRepository.findByUsername("username")).thenReturn(user);
 
@@ -41,11 +44,12 @@ class UserServiceTest {
         user.setUsername("username");
         user.setBirthday(LocalDate.parse("1999-12-12"));
         user.setGender(Gender.MALE);
-        user.setUserWeight(Map.of(LocalDate.now(), 76.7));
+        user.setUserWeight(Map.of(LocalDate.now(), 96.7));
+        user.setBodysize(176.0);
 
         when(userRepository.findByUsername("username")).thenReturn(user);
 
-        GymUser result = userService.addPersonalData("username", Gender.MALE, "1999-12-12", 76.7);
+        GymUser result = userService.addPersonalData("username", Gender.MALE, "1999-12-12", 96.7, 176.0);
 
         assertEquals(user, result);
     }
