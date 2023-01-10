9958168eb227ea2788f9eb557119ec709350632a
marzad <maschatz@gmx.net>
2023-01-10 16:01:30 +0100

    Change, add & remove elements

--- frontend/src/pages/HeadPage.tsx
+++ frontend/src/pages/HeadPage.tsx
@@ -1,8 +1,14 @@
 import {useNavigate} from "react-router-dom";
 import "../css/HeadPage.css"
+import React from "react";
+
+type HeadPageProps ={
+    username : string | undefined
+}
 
 
-export default function HeadPage(){
+export default function HeadPage(props: HeadPageProps){
+
     const navigate = useNavigate()
 
     function handlePersonalDataNavigation(){
@@ -23,10 +29,13 @@ export default function HeadPage(){
 
     return(
         <section className={"Buttons"}>
-            <button onClick={handlePersonalDataNavigation}>Persönliche Angaben</button><br/>
-            <button onClick={handleExercisesNavigation}>Übungen</button><br/>
-            <button onClick={handleWeekPlanNavigation}>Wochenplan</button><br/>
-            <button onClick={handleCalendarNavigation}>Kalender</button>
+            <h2>Hallo {props.username}!</h2>
+            <div>
+                <button onClick={handlePersonalDataNavigation}>Persönliche Angaben</button><br/>
+                <button onClick={handleExercisesNavigation}>Übungen</button><br/>
+                <button onClick={handleWeekPlanNavigation}>Wochenplan</button><br/>
+                <button onClick={handleCalendarNavigation}>Kalender</button>
+            </div>
         </section>
     )
 }
