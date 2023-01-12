d4cd8ea4992b47f318f0e7ebf0d1b618c6c1eee5
marzad <maschatz@gmx.net>
2023-01-12 14:34:50 +0100

    Add method to generate weekID

--- backend/src/main/java/trainingjournal/backend/service/IDGenerator.java
+++ backend/src/main/java/trainingjournal/backend/service/IDGenerator.java
@@ -2,6 +2,10 @@ package trainingjournal.backend.service;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.UUID;
 
 @Service
@@ -10,5 +14,12 @@ public class IDGenerator {
     public String getID(){
         UUID id = UUID.randomUUID();
         return id.toString();
+    }
+
+    public String getWeekID(){
+        Calendar newCalendar = new GregorianCalendar();
+        newCalendar.setTime(new Date());
+        int weekNumber = newCalendar.get(Calendar.WEEK_OF_YEAR);
+        return LocalDate.now().getYear() + "_" + weekNumber;
     }
 }
