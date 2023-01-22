1cfa3a8b37f28db688a9fc32cd742e64536d03fa
marzad <maschatz@gmx.net>
2023-01-22 01:11:37 +0100

    Add bmi display

--- frontend/src/components/UserWeightDetails.tsx
+++ frontend/src/components/UserWeightDetails.tsx
@@ -1,14 +1,13 @@
 
 type UserWeightDetailsProps={
-    userWeight : {date: Date, weight : number}
+    userWeight : {date: Date, weight : number, bmi: number}
 }
 
 export default function UserWeightDetails(props: UserWeightDetailsProps){
 
     return(
         <>
-            {props.userWeight.date} : {props.userWeight.weight}kg <br/>
-
+            {props.userWeight.date} : {props.userWeight.weight}kg BMI: {props.userWeight.bmi}<br/>
         </>
 
     )
