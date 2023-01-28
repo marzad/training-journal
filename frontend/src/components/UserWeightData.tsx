49a2204c8b9573a23386b5466f6a37aa53abdbea
marzad <maschatz@gmx.net>
2023-01-28 20:04:57 +0100

    Format table

--- frontend/src/components/UserWeightData.tsx
+++ frontend/src/components/UserWeightData.tsx
@@ -11,10 +11,20 @@ import {
     Legend,
 } from 'recharts';
 import "../css/PersonalData.css"
-import {Box, Button, FormControlLabel, TextField, Typography} from "@mui/material";
+import {
+    Box,
+    Button,
+    FormControlLabel,
+    Table, TableBody,
+    TableCell,
+    TableContainer, TableHead,
+    TableRow,
+    TextField,
+    Typography
+} from "@mui/material";
 
 type UserWeightDataProps = {
-    username : string
+    username: string
 }
 
 
@@ -39,24 +49,8 @@ export default function UserWeightData(props: UserWeightDataProps) {
         }
     }
 
-    const getUserWeightDetails = userWeightList.map(weightItems => {
-        return (
-            <table key={weightItems.date.toString()}>
-                <tbody>
-                <tr>
-                    <td>
-                        {weightItems.date.toString()}
-                    </td>
-                    <td>
-                        {weightItems.weight}
-                    </td>
-                    <td>
-                        {weightItems.bmi}
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        )
+    const rows = userWeightList.map(weightItems => {
+        return {date: weightItems.date.toString(), weight: weightItems.weight, bmi: weightItems.bmi}
     })
 
     function handleReturnOnClick() {
@@ -65,7 +59,7 @@ export default function UserWeightData(props: UserWeightDataProps) {
 
     const renderLineChart = () => {
         return (
-            <LineChart width={250} height={250} data={chartData}
+            <LineChart width={350} height={250} data={chartData}
                        margin={{top: 10, right: 30, left: 0, bottom: 0}}>
                 <defs>
                     <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -82,42 +76,56 @@ export default function UserWeightData(props: UserWeightDataProps) {
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip/>
                 <Legend/>
-                <Line type="monotone" dataKey="bmi" stroke="#8884d8" activeDot={{r: 8}}/>
-                <Line type="monotone" dataKey="weight" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="bmi" stroke="#8884d8" activeDot={{r: 8}} dot={true}/>
+                <Line type="monotone" dataKey="weight" stroke="#82ca9d"/>
             </LineChart>
         )
     }
 
     return (
         <Box component={"section"}>
-            <section>
+            <div>
                 <Typography variant={"h5"}>Gewicht und BMI</Typography>
                 {renderLineChart()}
-            </section>
+            </div>
             <form onSubmit={handleFormOnChange}>
                 <FormControlLabel control={<TextField type={"number"}
                                                       onChange={handleInputOnChange}
                                                       name={"userWeight"}
                                                       size={"small"}
-                                                      label={"Gewicht"}/>}
-                                  label={<Button type={"submit"}>Speichern</Button>}/>
+                                                      label={"Gewicht"}
+                                                      sx={{width: 100}}/>}
+                                  label={<Button type={"submit"}>Speichern</Button>}
+                                  sx={{margin: 1}}/>
 
             </form>
-            <form>
-                <table>
-                    <thead>
-                    <td>Datum</td>
-                    <td>Gewicht</td>
-                    <td>BMI</td>
-                    </thead>
-                </table>
-                {getUserWeightDetails}
-            </form>
+            <TableContainer>
+                <Table size={"small"}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Datum</TableCell>
+                            <TableCell>Gewicht</TableCell>
+                            <TableCell>BMI</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => {
+                            return (
+                                <TableRow key={row.date.toString()}>
+                                    <TableCell>{row.date} </TableCell>
+                                    <TableCell>{row.weight} </TableCell>
+                                    <TableCell>{row.bmi} </TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <Button variant={"outlined"}
+                    sx={{margin: 1}}
                     size={"small"}
                     onClick={handleReturnOnClick}
                     color={"success"}
-                    style={{margin: 5}}
             >zurück</Button>
         </Box>
     )
